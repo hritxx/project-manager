79d848abe1abfd93ed30af63686b558e9f18abc8
hritxx <hriteekroy1869@gmail.com>
2024-12-24 21:39:29 +0530

    v0.0.0

---
+++ client/src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <main className="row-start-2 flex flex-col items-center gap-8">hi</main>
+  );
+}
---
+++ server/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import projectRoutes from "./routes/projectRoutes";
+
+//ROUTE IMPORTS
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("This is the home route.");
+});
+app.use("/projects", projectRoutes);
+
+// SERVERS
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
