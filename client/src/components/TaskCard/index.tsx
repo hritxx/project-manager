a1d7108e0c481006c6d81a7f56b325c98a46e538
hritxx <hriteekroy1869@gmail.com>
2025-01-04 18:43:37 +0530

    board-view & list-view

---
+++ client/src/components/TaskCard/index.tsx
@@ -0,0 +1,56 @@
+import { Task } from "@/state/api";
+import { format } from "date-fns";
+import Image from "next/image";
+
+type Props = {
+  task: Task;
+};
+
+const index = ({ task }: Props) => {
+  return (
+    <div className="mb-5 rounded bg-white p-4 shadow dark:bg-dark-secondary dark:text-white">
+      {task.attachments && task.attachments.length > 0 && (
+        <div>
+          <strong>Attachments:</strong>
+          {task.attachments && task.attachments.length > 0 && (
+            <Image
+              src={`/${task.attachments[0].fileURL}`}
+              alt={`/${task.attachments[0].fileName}`}
+              width={400}
+              height={200}
+              className="rounded-md"
+            />
+          )}
+        </div>
+      )}
+      <p>
+        <strong>ID:</strong>
+        {task.id}
+      </p>
+      <p>
+        <strong>Title:</strong> {task.title}
+      </p>
+      <p>
+        <strong>Description:</strong>{" "}
+        {task.description ? task.description : "No description provided"}
+      </p>
+      <p>
+        <strong>Status:</strong>
+        {task.status}
+      </p>
+      <p>
+        <strong>Tags:</strong> {task.tags ? task.tags : "No tags"}
+      </p>
+      <p>
+        <strong>Start Date:</strong>
+        {task.startDate ? format(new Date(task.startDate), "P") : "Not set"}
+      </p>
+      <p>
+        <strong>End Date:</strong>
+        {task.dueDate ? format(new Date(task.dueDate), "P") : "Not set"}
+      </p>
+    </div>
+  );
+};
+
+export default index;
